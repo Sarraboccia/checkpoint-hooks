b2f46acdbdb203d6eede70d34622f117a70f60da
Sara Boccia <saraboccia@Saras-MacBook-Air.local>
2021-11-19 22:49:47 +0100

    router

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import Filter from "./Components/Filter/Filter";
 import Movielist from "./Components/MoviesList/Movieslist";
 import NavBar from "./Components/NavBar/NavBar";
 import { data } from "./data.js";
+import Moviecard from "./Components/Moviecard/Moviecard";
+import Moviedetails from "./Components/Moviedetails/Moviedetails";
+import {Route} from "react-router-dom"
 function App() {
   
 const [textFilter, setTextFilter] = useState("")
@@ -15,9 +18,9 @@ const [starsRate, setStarsRate] = useState(0)
    }, [])
 
   const [movies, setMovies] = useState(data);
-
   const addMovie = (newMovie) => {
     setMovies([...movies, newMovie]);
+    
   };
 
   return (
@@ -26,8 +29,30 @@ const [starsRate, setStarsRate] = useState(0)
       <NavBar addMovie={addMovie}/>
       
       <Filter setTextFilter={setTextFilter} setStarsRate={setStarsRate}/>
-      <Movielist movies={movies} textFilter={textFilter} starsRate={starsRate}/>
+
+    
+      <Route
+          exact
+          path="/"
+          render={() => (
+            <Movielist movies={movies} textFilter={textFilter} starsRate={starsRate}/>
+
+          )}
+        />
+
+        <Route
+          exact
+          path="/movie/moviedetails/:myid"
+          render={(defaultProps) => (
+            <Moviedetails {...defaultProps} movies={movies} />
+          )}
+        />
+     
+      
+    
     </div>
+
+
   );
 }
 
--- src/data.js
+++ src/data.js
@@ -4,23 +4,29 @@ export const data = [
   {
     id: uuidv4(),
     title: "Cinderella",
-    photo:
-      "https://m.media-amazon.com/images/M/MV5BZTk3ZTEzNGUtZTcwYy00NmRmLWFhMGYtZjA4NWY1ZWI4MzMyXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_.jpg",
+    photo: "https://m.media-amazon.com/images/M/MV5BZTk3ZTEzNGUtZTcwYy00NmRmLWFhMGYtZjA4NWY1ZWI4MzMyXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_.jpg",
     rate: 2,
+    trailer : "https://www.youtube.com/embed/VDB3onzBMfI",
+    description : "Cinderella is a modern movie musical with a bold take on the classic fairytale. Our ambitious heroine Camila Cabello has big dreams and with the help of her Fab Godmother, she perseveres to make them come true. Cinderella has an all-star cast including Idina Menzel, Minnie Driver, James Corden, Nicholas Galitzine, Billy Porter and Pierce Brosnan",
+
   },
   {
     id: uuidv4(),
     title: "Dachra",
-    photo:
-      "https://m.media-amazon.com/images/M/MV5BZTFkYWZkNTUtZGJlOC00MmE1LWEzZmMtNGJhZWU1NDFiYzE5XkEyXkFqcGdeQXVyNjUyNjYzNTE@._V1_FMjpg_UX1000_.jpg",
+    photo: "https://m.media-amazon.com/images/M/MV5BZTFkYWZkNTUtZGJlOC00MmE1LWEzZmMtNGJhZWU1NDFiYzE5XkEyXkFqcGdeQXVyNjUyNjYzNTE@._V1_FMjpg_UX1000_.jpg",
     rate: 3,
+    trailer : "https://www.youtube.com/embed/iPSrkGu62lE",
+    description : "Three journalism students Yasmine Walid and Bilel set off to investigate a cold case for a school film project. After visiting a mental hospital to interview Mongia, the mutilated survivor of an attack 20 years before the trail leads them out to an isolated village. Before they fully understand what is happening, they find themselves beset by cannibal witches. The movie ends with the message of How the ghosts seeks its revenge",
+
   },
   {
     id: uuidv4(),
     title: "Birdbox",
-    photo:
-      "https://upload.wikimedia.org/wikipedia/en/b/bd/Bird_Box_%28film%29.png",
+    photo: "https://upload.wikimedia.org/wikipedia/en/b/bd/Bird_Box_%28film%29.png",
     rate: 5,
+    trailer : "https://www.youtube.com/embed/o2AsIXSh2xo",
+    description : "Five years after an ominous unseen presence drives most of society to suicide, a mother and her two children make a desperate bid to reach safety. Five years after an ominous unseen presence drives most of society to suicide, a mother and her two children make a desperate bid to reach safety",
+
   },
 
   {
@@ -29,25 +35,37 @@ export const data = [
     photo:
       "https://www.themoviedb.org/t/p/w500/cP7odDzzFBD9ycxj2laTeFWGLjD.jpg",
     rate: 4,
+    trailer : "https://www.youtube.com/embed/NiFuJV2GLkY",
+    description : "It's about a man trying to get revenge for his wife's death from cancer at the hands of a depraved and indifferent pharmaceutical company while his daughter (Isabela Merced) is pulled along by the currents of his rage",
+
   },
   {
     id: uuidv4(),
     title: "He's is all that",
-    photo:
-      "https://m.media-amazon.com/images/M/MV5BYzM3NmYyMTItZWY5My00MTQzLWFmMzEtOTc2ZjRkMDJkYWM3XkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_.jpg",
+    photo: "https://m.media-amazon.com/images/M/MV5BYzM3NmYyMTItZWY5My00MTQzLWFmMzEtOTc2ZjRkMDJkYWM3XkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_.jpg",
     rate: 5,
+    trailer : "https://www.youtube.com/embed/XqTPaRz8Nx8",
+    description : "A teenage girl sets out to give a nebbish classmate the ultimate high school makeover. A teenage girl sets out to give a nebbish classmate the ultimate high school makeover. ... An updated remake of the 1999 film, 'She's All That'",
+
   },
   {
     id: uuidv4(),
     title: "Hypnotic",
-    photo:
-      "https://i2.wp.com/download.24baze.com/wp-content/uploads/2021/10/Hypnotic-2021-Mp4-Netnaija.png",
+    photo: "https://i2.wp.com/download.24baze.com/wp-content/uploads/2021/10/Hypnotic-2021-Mp4-Netnaija.png",
+    rate : 3,
+    trailer : "https://www.youtube.com/embed/eHsWYmnXk1o",
+    description : "A young woman seeking self-improvement enlists the help of a renowned hypnotherapist. But after a handful of intense sessions, she discovers unexpected and deadly consequences. A young woman seeking self-improvement enlists the help of a renowned hypnotherapist",
+
   },
   {
     id: uuidv4(),
     title: "Miracle in cell no.7",
     photo: "https://fr.web.img4.acsta.net/pictures/19/10/08/15/52/1926679.jpg",
     rate: 5,
+    trailer :"https://www.youtube.com/embed/G2wPoBy2JQI",
+    description : "Separated from his daughter, a father with an intellectual disability must prove his innocence when he is jailed for the death of a commander's child",
+
+   
   },
   {
     id: uuidv4(),
@@ -55,6 +73,9 @@ export const data = [
     photo:
       "https://m.media-amazon.com/images/M/MV5BNjJkMzNjOTUtZjc3NC00ZjU1LWE0OTEtMGMxZTY5NzcxZjk3XkEyXkFqcGdeQXVyNTc5OTMwOTQ@._V1_.jpg",
     rate: 4,
+    trailer : "https://www.youtube.com/embed/k97BxtxXNE8",
+    description : "Two best friends with MBAs are bored in their jobs and start a travel business for bachelor parties in Budapest",
+
   },
   {
     id: uuidv4(),
@@ -62,13 +83,18 @@ export const data = [
     photo:
       "https://m.media-amazon.com/images/M/MV5BY2ViODRiN2EtMGM3Zi00MjA1LTkyNDMtYTUzYTkxNTVhMGI0XkEyXkFqcGdeQXVyNzg5ODQxNDU@._V1_FMjpg_UX1000_.jpg",
     rate: 3,
+    trailer : "https://www.youtube.com/embed/9-voNCYrc10",
+    description : "It tells a typical romantic love story between Thibault (Mickael Lumiere, voiced by Jordan Tortorello), a righteous man who wants to get out of a platonic relationship. While attending a Bachelor party, he meets Rose (Nada Belka), a designer for baby clothes that happens to break free from a troublesome ex-boyfriend",
+
   },
   {
     id: uuidv4(),
-    photo:
-      "https://www.cinehorizons.net/sites/default/files/affiches/514025354-army-thieves.jpg",
     title: "Army of thives",
+    photo:"https://www.cinehorizons.net/sites/default/files/affiches/514025354-army-thieves.jpg",
     rate: 4,
+    trailer : "https://www.youtube.com/embed/Ith2WetKXlg",
+    description : "A prequel, set before the events of Army of the Dead, which focuses on German safecracker Ludwig Dieter leading a group of aspiring thieves on a top secret heist during the early stages of the zombie apocalypse. More Safes. Less Zombies",
+
   },
   {
     id: uuidv4(),
@@ -76,24 +102,35 @@ export const data = [
     photo:
       "https://m.media-amazon.com/images/M/MV5BMzllNmRjYWItNjlhNS00MjcyLWIyODEtMTg0ZjBiNTNhNzQwXkEyXkFqcGdeQXVyMTAyOTE2ODg0._V1_FMjpg_UX1000_.jpg",
     rate: 5,
+    trailer : "https://www.youtube.com/embed/Pb40NLXBHF4",
+    description : "The Spanish-language début feature from director Galder Gaztelu-Urrutia is a satirical-sci-fi-cum-horror-drama that strains to make social-commentating points about capitalism and classism amid toilet jokes and cannibalistic threats",
+
   },
   {
     id: uuidv4(),
     title: "Night teeth",
     photo: "https://lookmovie.club/wp-content/uploads/2021/10/Night-Teeth-2021.jpg",
     rate: 3,
+    trailer : "https://www.youtube.com/embed/ZNu-G-rd4H4",
+    description : "Moonlights as a chauffeur for one night. His task: drive two mysterious young women (Debby Ryan and Lucy Fry) around Los Angeles for a night of party hopping. Taken captive by his clients' charm, he soon learns that his passengers have their own plans for him -- and an insatiable thirst for blood",
+
   },
   {
     id: uuidv4(),
     title: "Paper lives",
     photo: "https://pbs.twimg.com/media/EyIml9IXEAI1bAn.jpg",
     rate: 4,
+    trailer : "https://www.youtube.com/embed/lXdSaVK-vzs",
+    description : "In an impoverished neighborhood of Istanbul, a friendly garbage collector, Mehmet, who after being homeless has a soft spot for the many homeless children in the area, discovers an eight-year-old boy hiding in his colleague's garbage bag. Mehmet is dedicated to reconnecting the boy with his family",
+
   },
   {
     id: uuidv4(),
     title: "Yara",
-    photo:
-      "https://m.media-amazon.com/images/M/MV5BMWFhYzE3YTUtZTVhNC00NWNkLWFkNzUtOTM5MTcxMDdhNzMzXkEyXkFqcGdeQXVyMTQ3Njg3MQ@@._V1_.jpg",
+    photo: "https://m.media-amazon.com/images/M/MV5BMWFhYzE3YTUtZTVhNC00NWNkLWFkNzUtOTM5MTcxMDdhNzMzXkEyXkFqcGdeQXVyMTQ3Njg3MQ@@._V1_.jpg",
     rate: 3,
-  },
+    trailer : "https://www.youtube.com/embed/qXxKhYFAsPk",
+    description : "Yara. A film which documents the tragic murder of a 13-year old girl in Italy, named Yara Gambirasio. The film stars Isabella Ragonese, Alessio Boni and Chiara Bono, and revolves around the prosecutor who gives everything she has in order to solve the case",
+
+  }
 ];
